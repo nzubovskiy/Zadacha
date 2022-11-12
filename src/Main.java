eddb935412d1e559de82e44f742ac29c9ad5d199
NIKO <nzubovskiy@gmail.com>
2022-11-12 17:09:58 +0400

    Решение задачи из 10го билета

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] str = {"bb", "bb", "cc", "aa", "cc", "dd"};
+        Set<String> set = new HashSet<>(Arrays.asList(str));
+        String[] result = set.toArray(new String[set.size()]);
+
+        System.out.println(String.join("", result));
+
+
     }
 }
